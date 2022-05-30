a321a1e1f45ba16f65bfdb84b2b9c47571e06c31
Zach Gallagher <zachagallagher@gmail.com>
2022-05-30 12:10:46 -0700

    add instruction, processor, error, state rust files

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,3 @@
 pub mod entrypoint;
+pub mod instruction;
+pub mod processor;
---
+++ src/state.rs
@@ -0,0 +1,50 @@
+use solana_program::{
+  program_pack::{IsInitialized, Pack, Sealed},
+  program_error::ProgramError,
+  pubkey::Pubkey,
+};
+
+use arrayref::{array_mut_ref, array_ref, array_refs, mut_array_refs};
+
+pub struct Escrow {
+    pub is_initialized: bool,
+    pub initializer_pubkey: Pubkey,
+    pub temp_token_account_pubkey: Pubkey,
+    pub initializer_token_to_receive_account_pubkey: Pubkey,
+    pub expected_amount: u64,
+}
+
+impl Sealed for Escrow {}
+
+impl IsInitialized for Escrow {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+}
+
+impl Pack for Escrow {
+  const LEN: usize = 105;
+  fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
+      let src = array_ref![src, 0, Escrow::LEN];
+      let (
+          is_initialized,
+          initializer_pubkey,
+          temp_token_account_pubkey,
+          initializer_token_to_receive_account_pubkey,
+          expected_amount,
+      ) = array_refs![src, 1, 32, 32, 32, 8];
+      let is_initialized = match is_initialized {
+          [0] => false,
+          [1] => true,
+          _ => return Err(ProgramError::InvalidAccountData),
+      };
+
+      Ok(Escrow {
+          is_initialized,
+          initializer_pubkey: Pubkey::new_from_array(*initializer_pubkey),
+          temp_token_account_pubkey: Pubkey::new_from_array(*temp_token_account_pubkey),
+          initializer_token_to_receive_account_pubkey: Pubkey::new_from_array(*initializer_token_to_receive_account_pubkey),
+          expected_amount: u64::from_le_bytes(*expected_amount),
+      })
+  }
+}
